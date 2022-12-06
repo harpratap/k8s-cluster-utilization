de02daf36303bf563a3d66bc99df4744195d202f
Harpratap <harpratap@mercari.com>
2022-12-06 15:03:55 +0900

    add node resource usage stats

--- cmd/resource.go
+++ cmd/resource.go
@@ -33,10 +33,52 @@ var resourceNamespaceCmd = &cobra.Command{
 	},
 }
 
+var resourceNodeCmd = &cobra.Command{
+	Use:   "node",
+	Short: "Calculate resource usage stats per Node",
+	Long:  `Segregates info on Allocatable, daemonsets and pods CPU requests and usage`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		nUtil, err := kc.NodeUsage(context.Background(), args[0])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Printf(`Total CPU: %.2f
+Total Memory: %.2f
+Allocatable CPU: %.2f (%.2f%%)
+Allocatable Memory: %.2f (%.2f%%)
+Requested CPU: %.2f (%.2f%%)
+Requested Memory: %.2f (%.2f%%)
+Used CPU:
+Used Memory:
+Daemonset CPU: %.2f (%.2f%%)
+Daemonset Memory: %.2f (%.2f%%)
+Workload CPU: %.2f (%.2f%%)
+Workload Memory: %.2f (%.2f%%)
+Allocatable Pods: %d
+Running Pods: %d (%d%%)
+`,
+			nUtil.TotalCPU,
+			nUtil.TotalMemory,
+			nUtil.AllocatableCPU, nUtil.AllocatableCPU*100/nUtil.TotalCPU,
+			nUtil.AllocatableMemory, nUtil.AllocatableMemory*100/nUtil.TotalMemory,
+			nUtil.RequestedCPU, nUtil.RequestedCPU*100/nUtil.AllocatableCPU,
+			nUtil.RequestedMemory, nUtil.RequestedMemory*100/nUtil.AllocatableMemory,
+			nUtil.DaemonsetCPU, nUtil.DaemonsetCPU*100/nUtil.AllocatableCPU,
+			nUtil.DaemonsetMemory, nUtil.DaemonsetMemory*100/nUtil.AllocatableMemory,
+			nUtil.WorkloadCPU, nUtil.WorkloadCPU*100/nUtil.AllocatableCPU,
+			nUtil.WorkloadMemory, nUtil.WorkloadMemory*100/nUtil.AllocatableMemory,
+			nUtil.AllocatablePods,
+			nUtil.RunningPods, nUtil.RunningPods*100/nUtil.AllocatablePods)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(resourceCmd)
 	resourceCmd.Flags().StringVarP(&exceptionNS, "exception-namespaces", "x", "", "comma separated names of namespaces to exclude")
 	resourceCmd.PersistentFlags().BoolVarP(&skipBestEffort, "skip-best-effort-pods", "s", false, "skip pods with no cpu OR memory requests")
 
 	resourceCmd.AddCommand(resourceNamespaceCmd)
+	resourceCmd.AddCommand(resourceNodeCmd)
 }
